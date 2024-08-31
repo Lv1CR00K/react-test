1cad5a71c68e2e3aefb5c85419321a006459c29a
Lv1CR00K <brainvirus990@gmail.com>
2024-08-31 21:40:18 +0800

    Bug Fix Increment Count 1.8

--- src/VelOne/crookwork_icon.jsx
+++ src/VelOne/crookwork_icon.jsx
@@ -25,6 +25,7 @@ export default function BtnIcons(props){
         if (isFirstRender.current) {
             isFirstRender.current = false;
             glIconCount += 1;
+            console.log(glIconCount);
           }
       }, []); 
     const count = glIconCount;
