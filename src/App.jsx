b5c03b0b03180372cbdf77a2ee6e746345714470
Lv1CR00K <brainvirus990@gmail.com>
2024-09-01 17:06:04 +0800

    Color

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ export default function App(){
         {/* title="_nope" for no animation */}
         <div className='flex_row' style={{ padding : '30px' }}>
             <BtnIcons type='bell' Mar="10"/>
-            <BtnIcons type='menu' Mar="10"/>
-            <BtnIcons type='mess' Mar="10"/>
-            <BtnIcons type='pass' Mar="10"/>
-            <BtnIcons type='prof' Mar="10"/>
+            <BtnIcons type='menu' Mar="10" bRad="5" bgpa="blue"/>
+            <BtnIcons type='mess' Mar="10" bRad="10" bgpa="red"/>
+            <BtnIcons type='pass' Mar="10" bRad="15" bgpa="green"/>
+            <BtnIcons type='prof' Mar="10" bRad="20" bgpa="violet"/>
         </div>
     </>
     );
