f18531fc32f6fe5560f4f57549859749721ab1e0
Lv1CR00K <brainvirus990@gmail.com>
2024-09-02 15:29:40 +0800

    Logo Fixer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 export default function Header({logo, title, itemsAry}){
     return(
         <header>
-            <img src="../../crookwork.png" style={{width : "40px", margin: "10px"}}/>
+            <img src="./crookwork.png" style={{width : "40px", margin: "10px"}}/>
         </header>
     );
 }
