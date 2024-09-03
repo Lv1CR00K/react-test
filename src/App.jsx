6be4b355dd6b8b9e2b78466e6c5d9e84b8654308
Lv1CR00K <brainvirus990@gmail.com>
2024-09-03 16:51:53 +0800

    Delos

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,27 @@ import BoxBody from './VelOne/components/box_body.jsx'
 import HeadsOne from './VelOne/components/h1.jsx'
 import './VelOne/crookwork.css'
 
+const TEXT = "Tang ina mo KENNETH DELOS REYES!";
 export default function App(){
     return (
     <>
         <Header />
         <div className='flex_column' style={{alignItems : "center"}}>
-        <BoxBody rad={10} type="BGSha">
-            <HeadsOne text="Test the BOX" type="h1"/>
-        </BoxBody>
+            <BoxBody rad={10}>
+                <HeadsOne text={TEXT} type="h1"/>
+            </BoxBody>
+            <BoxBody rad={10} type="BorBG">
+                <HeadsOne text={TEXT} type="h1"/>
+            </BoxBody>
+            <BoxBody rad={10} type="BorBGSha">
+                <HeadsOne text={TEXT} type="h1"/>
+            </BoxBody>
+            <BoxBody rad={10} type="BG">
+                <HeadsOne text={TEXT} type="h1"/>
+            </BoxBody>
+            <BoxBody rad={10} type="BGSha">
+                <HeadsOne text={TEXT} type="h1"/>
+            </BoxBody>
         </div>
         <Footer />
     </>
