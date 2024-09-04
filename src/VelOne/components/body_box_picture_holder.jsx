9da67e4158459578de626b0b7ddb09f22f908171
Lv1CR00K <brainvirus990@gmail.com>
2024-09-04 21:09:55 +0800

    Fix

--- src/VelOne/components/body_box_picture_holder.jsx
+++ src/VelOne/components/body_box_picture_holder.jsx
@@ -5,7 +5,7 @@ export default function BBPicHolder({type, objectStyle, title, src}){
     return(
         <BoxBody type={type} objectStyle={objectStyle}>
             <img src={src} style={{aspectRatio: "1/1", borderRadius: "15px"}}/>
-            <HeadsOne text={title} type="h3" objectStyle={{textAlign: "center", paddingBottom: "0px"}}/>
+            <HeadsOne text={title} type="h4" objectStyle={{textAlign: "center", paddingBottom: "0px"}}/>
         </BoxBody>
     );
 
